5ce6dc58ddd7bcded4d3f3e571aea7970d5e6697
Joy-FrontEnd <bmmuthonijoy@gmail.com>
2020-12-02 09:36:13 +0400

    updated weather icon

--- main.js
+++ main.js
@@ -17,20 +17,22 @@ function showDate(timestamp) {
 
 
 function showTemperature(response) {
-
+  
   let tempElement = document.querySelector(".temp");
   let cityElement = document.querySelector(".city");
   let weatherElement = document.querySelector(".weather");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector(".date");
+  let iconElement = document.querySelector("#weather-icons")
   tempElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   weatherElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = showDate(response.data.dt * 1000)
-
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "847fb93bdb350f3f8e4c62e543d7f8f1";
